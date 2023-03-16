046c54d8e29da68bb3fa64fc2bb7396162eddeac
wuxxing <2411147266@qq.com>
2023-03-16 11:05:42 +0800

    优化首屏加载适配黑暗模式

---
+++ js/index-legacy-c1f4780c.js
@@ -0,0 +1,1 @@
+System.register(["./index-legacy-b4d7ae7e.js"],(function(e){"use strict";var t;return{setters:[function(e){t=e.R}],execute:function(){e({h:function(e){return String(e).replace(/(\d{3})\d{4}(\d{4})/,"$1****$2")},p:function(e){var n=arguments.length>1&&void 0!==arguments[1]?arguments[1]:t;return+e/Number(n)+"rem"}})}}}));
--- js/simple-swizzle-76a3913f.js
+++
@@ -1,1 +0,0 @@
-import{i as r}from"./is-arrayish-aa096d02.js";var t={exports:{}},a=r,o=Array.prototype.concat,n=Array.prototype.slice,e=t.exports=function(r){for(var t=[],e=0,p=r.length;e<p;e++){var s=r[e];a(s)?t=o.call(t,n.call(s)):t.push(s)}return t};e.wrap=function(r){return function(){return r(e(arguments))}};export{t as s};
