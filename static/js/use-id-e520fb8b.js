3529592e38dc8de74606462f733d1a73e67bdef5
吴小星 <2411147266@qq.com>
2023-08-07 21:21:16 +0800

    feat: 优化部分页面细节

--- static/js/use-id-e520fb8b.js
+++
@@ -1,1 +0,0 @@
-import{ak as e}from"./index-3fa081e2.js";let r=0;function u(){const n=e(),{name:t="unknown"}=(n==null?void 0:n.type)||{};return`${t}-${++r}`}export{u};
--- static/js/use-sync-prop-ref-f25596ec.js
+++
@@ -1,1 +0,0 @@
-import{a as c,w as a}from"./index-3fa081e2.js";const n=(s,o)=>{const r=c(s());return a(s,f=>{f!==r.value&&(r.value=f)}),a(r,f=>{f!==s()&&o(f)}),r};export{n as u};
